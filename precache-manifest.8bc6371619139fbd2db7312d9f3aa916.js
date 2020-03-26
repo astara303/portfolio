24a1fd96b3b7009ee9f81a069d7e26667cbf8512
Astara Cambata <AstaraCambata@gmail.com>
2020-03-26 10:03:18 +0000

    Updates

---
+++ precache-manifest.8bc6371619139fbd2db7312d9f3aa916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d1b5c2093336e39bc86e3fcb6219d5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "411d4a29020164a4099a",
+    "url": "/portfolio/static/css/2.b328fd89.chunk.css"
+  },
+  {
+    "revision": "caee2dba65e3322615c6",
+    "url": "/portfolio/static/css/main.d3b34a52.chunk.css"
+  },
+  {
+    "revision": "411d4a29020164a4099a",
+    "url": "/portfolio/static/js/2.fbe2ea5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fbe2ea5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caee2dba65e3322615c6",
+    "url": "/portfolio/static/js/main.2b76a4f1.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
